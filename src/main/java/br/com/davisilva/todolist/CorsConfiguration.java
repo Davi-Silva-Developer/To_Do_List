a77bba58a79707c5c95ea4fde381bb9c8edde7b5
Davi Silva Souza <Davi95silvasouza12@gmail.com>
2026-03-11 09:14:23 -0300

    Pequena refatoração no CorsConfiguration

--- src/main/java/br/com/davisilva/todolist/CorsConfiguration.java
+++ src/main/java/br/com/davisilva/todolist/CorsConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfiguration {
-
     @Configuration
     public class corsConfiguration implements WebMvcConfigurer {
 
@@ -17,4 +15,3 @@ public class CorsConfiguration {
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
         }
     }
-}
